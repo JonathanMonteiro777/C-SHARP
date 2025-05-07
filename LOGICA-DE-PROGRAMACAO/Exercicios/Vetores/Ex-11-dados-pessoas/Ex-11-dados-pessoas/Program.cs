7f37ed6c994f481390cdc6c573a85b23437f636c
JonathanMonteiro777 <167100917+JonathanMonteiro777@users.noreply.github.com>
2025-05-06 22:06:45 -0300

    Atualizando exercicios na pasta de vetores

---
+++ LOGICA-DE-PROGRAMACAO/Exercicios/Vetores/Ex-11-dados-pessoas/Ex-11-dados-pessoas/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    public class DadosPessoas
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Quantas pessoas serao digitadas? ");
+            int N = int.Parse(Console.ReadLine());
+
+            double[] alturas = new double[N];
+            string[] generos = new string[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write($"Altura da {i + 1}a pessoa: ");
+                alturas[i] = double.Parse(Console.ReadLine(), CI);
+                Console.Write($"Genero da {i + 1}a pessoa: ");
+                generos[i] = Console.ReadLine().ToLower();
+            }
+
+            // Inicializa menorAltura e maiorAltura com o primeiro elemento
+            double menorAltura = alturas[0];
+            double maiorAltura = alturas[0];
+
+            for (int i = 1; i < N; i++)
+            {
+                if (alturas[i] < menorAltura)
+                {
+                    menorAltura = alturas[i];
+                }
+                if (alturas[i] > maiorAltura)
+                {
+                    maiorAltura = alturas[i];
+                }
+            }
+            Console.WriteLine($"Menor altura = {menorAltura.ToString("F2", CI)}");
+            Console.WriteLine($"Maior altura = {maiorAltura.ToString("F2", CI)}");
+
+            int contMulheres = 0;
+            int contHomens = 0;
+            double somaAlturasMulheres = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (generos[i] == "f")
+                {
+                    somaAlturasMulheres += alturas[i];
+                    contMulheres++;
+                }
+                else if (generos[i] == "m")
+                {
+                    contHomens++;
+                }
+            }
+
+            double mediaMulheres = (contMulheres > 0) ? (somaAlturasMulheres / contMulheres) : 0;
+            Console.WriteLine($"Media das alturas das mulheres = {mediaMulheres.ToString("F2", CI)}");
+            Console.WriteLine($"Numero de homens = {contHomens}");
+        }
+    }
+}
---
+++ LOGICA-DE-PROGRAMACAO/Exercicios/Vetores/Ex-12-comerciante/Ex-12-comerciante/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    public class Comerciante
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Serao digitados dados de quantos produtos? ");
+            int N = int.Parse(Console.ReadLine());
+
+            string[] nomes = new string[N];
+            double[] valorCompra = new double[N];
+            double[] valorVenda = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine($"Produto {i + 1}:");
+                Console.Write("Nome: ");
+                nomes[i] = Console.ReadLine();
+                Console.Write("Preco de compra: ");
+                valorCompra[i] = double.Parse(Console.ReadLine(), CI);
+                Console.Write("Preco de venda: ");
+                valorVenda[i] = double.Parse(Console.ReadLine(), CI);
+            }
+
+            // calcular a porcentagem de lucro dos produtos
+            int lucroBaixo = 0;
+            int lucroMedio = 0;
+            int lucroAcima = 0;
+            double lucroMercadoria;
+            double porcentagemLucro;
+
+            for (int i = 0; i < N; i++)
+            {
+                lucroMercadoria = valorVenda[i] - valorCompra[i];
+                porcentagemLucro = (lucroMercadoria / valorCompra[i]) * 100.0;
+
+                if (porcentagemLucro < 10.0)
+                {
+                    lucroBaixo++;
+                }
+                else if (porcentagemLucro >= 10.0 && porcentagemLucro <= 20.0)
+                {
+                    lucroMedio++;
+                }
+                else
+                {
+                    lucroAcima++;
+                }
+            }
+
+            // calcular o valor total dos produtos
+            double totCompra = 0.0;
+            double totVenda = 0.0;
+
+            for (int i = 0; i < N; i++)
+            {
+
+                totCompra += valorCompra[i];
+                totVenda += valorVenda[i];
+            }
+            double totLucro = totVenda - totCompra;
+
+            Console.WriteLine("\nRELATORIO:");
+            Console.WriteLine($"Lucro abaixo de 10%: {lucroBaixo}");
+            Console.WriteLine($"Lucro etre 10% e 20%: {lucroMedio}");
+            Console.WriteLine($"Lucro acima de 20%: {lucroAcima}");
+            Console.WriteLine($"Valor total de compra: {totCompra.ToString("F2", CI)}");
+            Console.WriteLine($"Valor total de venda: {totVenda.ToString("F2", CI)}");
+            Console.WriteLine($"Lucro total: {totLucro.ToString("F2", CI)}");
+        }
+    }
+}
